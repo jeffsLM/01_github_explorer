4be8cf590c80390aca313d9cf89b4549c86ccadb
jeffsLM <jefferson8841@gmail.com>
2021-07-09 20:41:27 -0300

    add final module

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,4 @@
-
-
+import './styles/global.scss'
+export function App() { 
+    return <h1>hello</h1>;
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,6 @@
 import {render} from 'react-dom';
 import {App} from './App'
 
-render(<h1>hello</h1>,document.getElementById("root"))
+
+
+render(<App/>,document.getElementById("root"))
